0b95f8797c3e8adbe0b4d1020dc88b1a8aa9e152
ghdrms1992 <ghdrms1992@gmail.com>
2021-01-19 18:54:04 +0900

    210119 commit

--- src_bak/index.js
+++ src_bak/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'index.css';
 import App from 'Components/App';
-import reportWebVitals from './reportWebVitals';
-import "./api";
-
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
